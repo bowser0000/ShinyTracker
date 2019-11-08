b2a6577fa92361d1a657b6f3d54ba405b7874998
bowser0000 <brahmjotgrewal@hotmail.com>
2019-11-08 18:42:02 -0500

    Fix shiny charm checkbox not being set attempt 3

--- script.js
+++ script.js
@@ -825,7 +825,7 @@ window.onload = function() {
     document.getElementById("count").textContent = parseInt(getCookie("countsave"));
     document.getElementById("multi").value = parseInt(getCookie("amountsave"));
     document.getElementById("method").value = getCookie("methodsave");
-    if (getCookie("shinycharmsave") == "true") document.getElementById("shinycharm").attr("checked", "checked");
+    if (getCookie("shinycharmsave") == "on") document.getElementById("shinycharm").attr("checked", "checked");
     document.getElementById("custom").value = parseInt(getCookie("customsave"));
     document.getElementById("pokemon1").value = getCookie("pkmn1save");
     document.getElementById("pokemon2").value = getCookie("pkmn2save");
